d76622bd88b7fb142bb1ebdb276cbb63fbcce509
Andreas Linz <klingt.net@gmail.com>
2016-02-27 14:21:01 +0100

    Add Ci struct

--- src/lib.rs
+++ src/lib.rs
@@ -22,21 +22,50 @@ macro_rules! err {
     }
 }
 
-fn ci() -> bool {
-    err!(env::var("CI")).is_some() ||
-    err!(env::var("CONTINUOUS_INTEGRATION")).is_some()
+struct Ci {
+    service: CiService,
 }
+impl Ci {
+    fn new() -> Option<Self> {
+        let s = Ci::which_ci();
+        match s {
+            CiService::Travis | CiService::Circle => Some(Ci { service: s }),
+            _ => None,
+        }
+    }
 
-fn which_ci() -> CiService {
-    match (err!(env::var("TRAVIS")), err!(env::var("CIRCLECI"))) {
-        (Some(_), None) => CiService::Travis,
-        (None, Some(_)) => CiService::Circle,
-        _ => CiService::Unknown,
+    fn which_ci() -> CiService {
+        match (err!(env::var("TRAVIS")), err!(env::var("CIRCLECI"))) {
+            (Some(_), None) => CiService::Travis,
+            (None, Some(_)) => CiService::Circle,
+            _ => CiService::Unknown,
+        }
+    }
+
+    fn is_travis(&self) -> bool {
+        match self.service {
+            CiService::Travis => true,
+            _ => false,
+        }
+    }
+
+    fn is_circle(&self) -> bool {
+        match self.service {
+            CiService::Circle => true,
+            _ => false,
+        }
     }
 }
 
+fn ci() -> bool {
+    err!(env::var("CI")).is_some() || err!(env::var("CONTINUOUS_INTEGRATION")).is_some()
+}
+
 #[test]
-fn ci_test() {
-    assert!(!ci());
-    assert_eq!(which_ci(), CiService::Unknown);
+fn ci_test_unknown() {
+    let ci = Ci::new();
+    if ci.is_some() {
+        return;
+    }
+    assert_eq!(Ci::which_ci(), CiService::Unknown);
 }
