b4c5f1606278b8285aa2ec2aa96530acd902a53a
Andreas Linz <klingt.net@gmail.com>
2016-02-27 14:03:37 +0100

    s/Ci/CiService/g

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod circle;
 use std::env;
 
 #[derive(PartialEq, Debug)]
-enum Ci {
+enum CiService {
     Travis,
     Circle,
     Unknown,
@@ -23,19 +23,20 @@ macro_rules! err {
 }
 
 fn ci() -> bool {
-    err!(env::var("CI")).is_some()
+    err!(env::var("CI")).is_some() ||
+    err!(env::var("CONTINUOUS_INTEGRATION")).is_some()
 }
 
-fn which_ci() -> Ci {
+fn which_ci() -> CiService {
     match (err!(env::var("TRAVIS")), err!(env::var("CIRCLECI"))) {
-        (Some(_), None) => Ci::Travis,
-        (None, Some(_)) => Ci::Circle,
-        _ => Ci::Unknown,
+        (Some(_), None) => CiService::Travis,
+        (None, Some(_)) => CiService::Circle,
+        _ => CiService::Unknown,
     }
 }
 
 #[test]
 fn ci_test() {
     assert!(!ci());
-    assert_eq!(which_ci(), Ci::Unknown);
+    assert_eq!(which_ci(), CiService::Unknown);
 }
